8c68ae4efcddd83562a2a6d7cfe09b99121ef77f
Cory Vernon <cory.vernon@live.com>
2021-01-14 12:58:56 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cookiecutter NodeJS "From Scratch" Quick Start Application
+
+A cookiecutter template to create a NodeJS Basic Quick Start Application using [Serverless Application Model (SAM)](https://github.com/awslabs/serverless-application-model).
+
+## Requirements
+
+* [AWS SAM CLI](https://github.com/awslabs/aws-sam-cli)
---
+++ {{cookiecutter.project_name}}/__tests__/unit/lambda.test.js
@@ -0,0 +1,17 @@
+// Import all functions from hello-from-lambda.js
+const lambda = require('../../src/lambda.js');
+
+// This includes all tests for helloFromLambdaHandler()
+describe('Test for default lambda function', function () {
+    // This test invokes lambda.handler() and compares the result 
+    it('Verifies successful response', async () => {
+        // Invoke helloFromLambdaHandler()
+        const result = await lambda.handler();
+
+        //  Assemble expected result
+        const expectedResult = 'Hello from Lambda!';
+
+        // Compare the result with the expected result
+        expect(result).toEqual(expectedResult);
+    });
+});
